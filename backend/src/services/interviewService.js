5ac9eb06eeb7a5a6c18f686a46e6cf1a32ed45b0
sumit-nagrikar <sumitnagrika1@gmail.com>
2025-07-03 16:06:36 +0530

    remove console

--- backend/src/services/interviewService.js
+++ backend/src/services/interviewService.js
@@ -221,7 +221,7 @@ export class InterviewService {
     if (!session) throw new AppError("Session not found", 400);
 
     try {
-      console.log('Generating final feedback, Chat History:', session.chatHistory);
+      // console.log('Generating final feedback, Chat History:', session.chatHistory);
       /* const result = await this.aiService.generateFinalAssessment(
         session.chatHistory.map((msg) =>
           msg.role === "human"

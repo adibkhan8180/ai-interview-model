6a9b93e64f18f762caa130d17e4e35ff2127e00d
Adib Khan <adib@truscholar.io>
2025-07-10 14:56:54 +0530

    Adib | feat(PDF): Changes the styling of the final assessment pdf

--- frontend/components/response-input.tsx
+++ frontend/components/response-input.tsx
@@ -173,7 +173,7 @@ export function ResponseInput({
               ref={inputRef}
               value={textResponse}
               onChange={(e) => setTextResponse(e.target.value)}
-              onPaste={(e) => e.preventDefault()}
+              // onPaste={(e) => e.preventDefault()}
               minLength={minAnswerLength}
               maxLength={maxAnswerLength}
               className="ml-2 text-sm sm:text-base flex-1 sm:font-medium border-none outline-none shadow-none placeholder:text-[#919ECD] px-2 py-3 resize-none h-[40px]"
